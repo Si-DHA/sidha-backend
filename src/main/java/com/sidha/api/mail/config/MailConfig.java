fca01e26e433154804807b6910fe7d5f864338cb
gabiiing13 <gabiiing13@gmail.com>
2024-03-02 12:36:36 +0700

    fix: reconfigure JavaMaillSender Attribute

---
+++ src/main/java/com/sidha/api/mail/config/MailConfig.java
@@ -0,0 +1,34 @@
+package com.sidha.api.mail.config;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.JavaMailSenderImpl;
+
+
+
+@Configuration
+public class MailConfig {
+
+  @Value("${spring.mail.host}")
+  private String host;
+
+  @Value("${spring.mail.port}")
+  private int port;
+
+  @Value("${spring.mail.username}")
+  private String username;
+
+  @Value("${spring.mail.password}")
+  private String password;
+
+  @Bean
+  public JavaMailSender javaMailSender() {
+    JavaMailSenderImpl mailSender = new JavaMailSenderImpl();
+    mailSender.setHost(host);
+    mailSender.setPort(port);
+    mailSender.setUsername(username);
+    mailSender.setPassword(password);
+    return mailSender;
+  }
+}
