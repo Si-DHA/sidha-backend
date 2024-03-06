9ac4f6a57a1d60f03124e61216052a1cc41ddfa7
Syifa Mumtaz Wazdy <syifa.mumtaz@ui.ac.id>
2024-03-06 13:58:46 +0700

    Pbi 7 mengelola data truk

---
+++ src/main/java/com/sidha/api/DTO/TrukMapper.java
@@ -0,0 +1,16 @@
+package com.sidha.api.DTO;
+
+import com.sidha.api.DTO.request.CreateTrukRequestDTO;
+import com.sidha.api.DTO.request.UpdateTrukRequestDTO;
+import com.sidha.api.model.Truk;
+import org.mapstruct.Mapper;
+
+@Mapper(componentModel = "spring")
+public interface TrukMapper {
+
+    Truk createTrukRequestDTOToTruk(CreateTrukRequestDTO createTrukRequestDTO);
+
+    Truk updateTrukRequestDTOToTruk(UpdateTrukRequestDTO updateTrukRequestDTO);
+
+
+}
