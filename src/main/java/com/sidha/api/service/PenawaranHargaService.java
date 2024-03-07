fbe383db4f62635506466a47733a38689c0131dc
Syifa Mumtaz Wazdy <syifa.mumtaz@ui.ac.id>
2024-03-07 15:11:25 +0700

    Pbi 8 mengelola daftar penawaran harga klien

--- src/main/java/com/sidha/api/service/PenawaranHargaService.java
+++ src/main/java/com/sidha/api/service/PenawaranHargaService.java
@@ -1,12 +1,17 @@
 package com.sidha.api.service;
 
+import com.sidha.api.DTO.request.CreatePenawaranHargaItemRequestDTO;
+import com.sidha.api.DTO.request.CreatePenawaranHargaRequestDTO;
 import com.sidha.api.model.PenawaranHarga;
 import com.sidha.api.model.PenawaranHargaItem;
+
+import jakarta.validation.Valid;
 
 import java.util.*;
 
 public interface PenawaranHargaService {
     PenawaranHarga getPenawaranHargaById(UUID idPenawaranHarga);
     List<PenawaranHarga> getAllPenawaranHarga();
+    PenawaranHarga createPenawaranHarga(CreatePenawaranHargaRequestDTO createPenawaranHargaRequestDTO);
     PenawaranHarga getPenawaranHargaByIdKlien(UUID klien);
 }
