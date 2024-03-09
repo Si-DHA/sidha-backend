59be740eb9b3ac9a370729af91ef79d881ae794b
Nur Fajar Sayyidul Ayyam <nur.fajar11@ui.ac.id>
2024-03-09 23:09:54 +0700

    style: remove unused import

--- src/main/java/com/sidha/api/controller/UserController.java
+++ src/main/java/com/sidha/api/controller/UserController.java
@@ -1,23 +1,11 @@
 package com.sidha.api.controller;
 
-import java.util.UUID;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RequestPart;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.multipart.MultipartFile;
-
-import com.sidha.api.DTO.UserMapper;
 import com.sidha.api.DTO.response.BaseResponse;
-import com.sidha.api.model.UserModel;
 import com.sidha.api.service.UserService;
-import java.util.*;
 
 @RequestMapping("/api/user")
 @RestController
@@ -25,11 +13,11 @@ public class UserController {
   @Autowired
   private UserService userService;
 
-  @Autowired
-  private UserMapper userMapper;
+  // @Autowired
+  // private UserMapper userMapper;
 
   @GetMapping("/klien")
-  public BaseResponse<List<UserModel>> getMethodName(@RequestParam String param) {
+  public BaseResponse<?> getMethodName() {
       return new BaseResponse<>(true, 200, "User list", userService.findByRole("KLIEN"));
   }
   
