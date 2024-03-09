d4f19490a409c6885da048ce207683bf30d298a2
Nur Fajar Sayyidul Ayyam <nur.fajar11@ui.ac.id>
2024-03-09 23:18:20 +0700

    fix: role

--- src/main/java/com/sidha/api/controller/UserController.java
+++ src/main/java/com/sidha/api/controller/UserController.java
@@ -5,6 +5,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import com.sidha.api.DTO.response.BaseResponse;
+import com.sidha.api.model.enumerator.Role;
 import com.sidha.api.service.UserService;
 
 @RequestMapping("/api/user")
@@ -18,7 +19,7 @@ public class UserController {
 
   @GetMapping("/klien")
   public BaseResponse<?> getMethodName() {
-      return new BaseResponse<>(true, 200, "User list", userService.findByRole("KLIEN"));
+      return new BaseResponse<>(true, 200, "User list", userService.findByRole(Role.KLIEN));
   }
   
 }
--- src/main/java/com/sidha/api/service/UserService.java
+++ src/main/java/com/sidha/api/service/UserService.java
@@ -1,12 +1,14 @@
 package com.sidha.api.service;
 
 import com.sidha.api.model.UserModel;
+import com.sidha.api.model.enumerator.Role;
+
 import java.util.List;
 public interface UserService {
 
     UserModel findByEmail(String email);
 
     UserModel findByUsername(String username);
-    List<UserModel> findByRole(String role);
+    List<UserModel> findByRole(Role klien);
     
 }
