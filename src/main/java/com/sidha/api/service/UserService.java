81988bdce84a3391bb8abd259569c668959e5a38
Gabriel Zebaoth Krisopras Putra <gabriel.zebaoth@ui.ac.id>
2024-04-01 19:11:16 +0700

    Staging

--- src/main/java/com/sidha/api/service/UserService.java
+++ src/main/java/com/sidha/api/service/UserService.java
@@ -9,13 +9,6 @@ import com.sidha.api.model.UserModel;
 import com.sidha.api.model.enumerator.Role;
 import java.util.List;
 
-
-import com.sidha.api.DTO.request.EditUserDetailRequestDTO;
-import com.sidha.api.DTO.response.GetUserDetailResponseDTO;
-import com.sidha.api.model.UserModel;
-import com.sidha.api.model.enumerator.Role;
-
-import java.util.List;
 public interface UserService {
 
     UserModel findByEmail(String email);
@@ -35,4 +28,6 @@ public interface UserService {
     List<UserModel> getListRole(Role role);
 
     List<Sopir> getListSopirNoTruk();
+    
+    List<UserModel> findAllList();
 }
