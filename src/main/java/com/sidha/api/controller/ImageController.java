6d17dd68e2a446fac09255dc82e24d44e07f3e3c
gabiiing13 <gabiiing13@gmail.com>
2024-04-03 23:46:06 +0700

    fix: setup user deletion to soft delete || findByRole can search all user, either deleted or not || chore: add api for get image by userid

--- src/main/java/com/sidha/api/controller/ImageController.java
+++ src/main/java/com/sidha/api/controller/ImageController.java
@@ -3,6 +3,7 @@ package com.sidha.api.controller;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.sidha.api.service.StorageService;
+import com.sidha.api.service.UserService;
 
 import java.io.IOException;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -12,20 +13,41 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.http.MediaType;
+import java.util.UUID;
 
 @RestController
 @RequestMapping("/api/image")
 public class ImageController {
 
   @Autowired
-  private StorageService service;
+  private StorageService storageService;
+
+  @Autowired
+  private UserService userService;
 
   @GetMapping("/{fileName}")
   public ResponseEntity<?> downloadImageFromFileSystem(@PathVariable String fileName) throws IOException {
-    byte[] imageData = service.getImageFromFileSystem(fileName);
+    byte[] imageData = storageService.getImageFromFileSystem(fileName);
     return ResponseEntity.status(HttpStatus.OK)
         .contentType(MediaType.valueOf("image/png"))
         .body(imageData);
+
+  }
+
+  @GetMapping("/file/{userId}")
+  public ResponseEntity<?> downloadImageFromFileSystemByUserId(@PathVariable String userId) throws IOException {
+    try {
+      var userUUID = UUID.fromString(userId);
+      var user = userService.findById(userUUID);
+      var fileName = user.getImageData().getName();
+      byte[] imageData = storageService.getImageFromFileSystem(fileName);
+      return ResponseEntity.status(HttpStatus.OK)
+          .contentType(MediaType.valueOf("image/png"))
+          .body(imageData);
+    } catch (Exception e) {
+      return ResponseEntity.status(HttpStatus.NOT_FOUND)
+          .body(e.getMessage());
+    }
 
   }
 }
