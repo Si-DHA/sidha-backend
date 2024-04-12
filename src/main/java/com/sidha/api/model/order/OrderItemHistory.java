360dcc0f3db3e51e9e5d5fbeccf3c81ef0a43651
Nur Fajar Sayyidul Ayyam <nur.fajar11@ui.ac.id>
2024-04-12 20:09:59 +0700

    fix: order item history model

--- src/main/java/com/sidha/api/model/order/OrderItemHistory.java
+++ src/main/java/com/sidha/api/model/order/OrderItemHistory.java
@@ -6,18 +6,24 @@ import com.fasterxml.jackson.annotation.JsonBackReference;
 import com.fasterxml.jackson.annotation.JsonFormat;
 
 import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.EqualsAndHashCode;
 import lombok.NoArgsConstructor;
 
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
+@EqualsAndHashCode(exclude = "orderItemId")
+@Entity
+@Table(name = "order_item_history")
 public class OrderItemHistory {
 
     @Id
