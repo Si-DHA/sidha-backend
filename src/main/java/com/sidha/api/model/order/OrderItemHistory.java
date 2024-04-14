7de78616030ecd51ed9457b3760fb63f76679397
Gabriel Zebaoth Krisopras Putra <gabriel.zebaoth@ui.ac.id>
2024-04-14 18:24:41 +0700

    Pbi 15 mengelola invoice

---
+++ src/main/java/com/sidha/api/model/order/OrderItemHistory.java
@@ -0,0 +1,50 @@
+package com.sidha.api.model.order;
+
+import java.time.LocalDateTime;
+
+import com.fasterxml.jackson.annotation.JsonBackReference;
+import com.fasterxml.jackson.annotation.JsonFormat;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@EqualsAndHashCode(exclude = "orderItem")
+@Entity
+@Table(name = "order_item_history")
+public class OrderItemHistory {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "order_item_id")
+    @JsonBackReference
+    private OrderItem orderItem;
+
+    @Column(name = "created_date", nullable = false, updatable = false)
+    private LocalDateTime createdDate = LocalDateTime.now();
+
+    @Column(name = "created_by", nullable = false, updatable = false)
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "dd-MM-yyyy hh:mm:ss")
+    private String createdBy;
+
+    @Column(name = "status", nullable = false)
+    private Integer status;
+
+    @Column(name = "description")
+    private String description;
+}
